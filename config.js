16d716b4967fc5111911c6e728ee09c01da97e99
peps0791 <piyush.papreja07@gmail.com>
2017-12-06 14:33:51 -0700

    changed config file for hosting

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
  
   app:{
     //dbUri: 'mongodb://peps0791:piyush0791@ds133814.mlab.com:33814/test_db',
-    //dbUri :'forumdbinstance.cwlujkspel9c.us-east-1.rds.amazonaws.com',
-    dbUri: 'localhost',
+    dbUri :'forumdbinstance.cwlujkspel9c.us-east-1.rds.amazonaws.com',
+    //dbUri: 'localhost',
     dbPort: 3306,
-    //dbUser: 'peps0791',
-    dbUser: 'root',
+    dbUser: 'peps0791',
+    //dbUser: 'root',
     dbPassword: 'piyush0791'
   }
 };
